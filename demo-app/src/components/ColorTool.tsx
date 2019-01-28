8604e62f314e476092bd578aa49de8b841524d64
John George <john@nepaweb.solutions>
2019-01-28 10:19:31 -0500

    Add Color tool

---
+++ demo-app/src/components/ColorTool.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const ColorTool = () => {
+
+    const colors =  [ 'blue', 'green', 'purple', 'pink', 'brown' ];
+
+    return <>
+        <header>
+            <h1>Color Tool</h1>
+        </header>
+        <ul>
+            {colors.map(color => <li key={color}>{color}</li>)}
+        </ul>
+    </>
+}
--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { HelloWorld } from './components/HelloWorld';
+import { CarTool } from './components/exercise-1/CarTool';
+import { ColorTool } from './components/ColorTool';
 
 ReactDOM.render(
-    <HelloWorld></HelloWorld>,
+    <>
+        <HelloWorld></HelloWorld>
+        <CarTool />
+        <ColorTool />
+    </>
+    ,
     document.querySelector('#root')
 );
